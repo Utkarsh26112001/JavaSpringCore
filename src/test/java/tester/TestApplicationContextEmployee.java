79fc0ba4ff9ad83922083782796aeba78bbbbc0a
Utkarsh26112001 <tiwari.utkarsh2611@gmail.com>
2023-01-31 15:43:38 +0530

    Updated Name

--- src/test/java/tester/TestApplicationContextEmployee.java
+++ src/test/java/tester/TestApplicationContextEmployee.java
@@ -4,7 +4,7 @@ import com.markets.bean.Employee;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class TestEmployee {
+public class TestApplicationContextEmployee {
     public static void main (String[] args){
         ApplicationContext ac= new ClassPathXmlApplicationContext("markets_config.xml");
         Employee emp=(Employee)ac.getBean("employee" );
